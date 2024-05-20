07814d1d2d927f75374e5d7524c2cb5132978e32
Greg Carter <gregamonintendo@gmail.com>
2024-05-20 17:30:16 +0100

    Fixing issue with eslint

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -8,7 +8,7 @@ import logo from "../../images/logo/fuji-digital-logo.svg";
 import textLogoWhite from "../../images/logo/fuji-digital-text-logo-white.svg";
 import textLogoPurple from "../../images/logo/fuji-digital-text-logo-purple.svg";
 
-export default ({ version, className }) => {
+export default Logo = ({ version, className }) => {
   let textLogo = version === "purple" ? textLogoPurple : textLogoWhite;
   return (
     <StyledLogoContainer className={className}>
--- src/components/SocialIcons/Icon.js
+++ src/components/SocialIcons/Icon.js
@@ -2,7 +2,7 @@ import LinkedInIcon from "./LinkedInIcon";
 import GitHubIcon from "./GitHubIcon";
 import { StyledIconContainer } from "./Icon.styles.js";
 
-export default ({ className }) => {
+export default Icon = ({ className }) => {
   return (
     <StyledIconContainer className={className}>
       <LinkedInIcon />
