ae40fc34c94298f19cd313f63c7ceb95757f0fe8
Greg Carter <gregamonintendo@gmail.com>
2024-06-24 15:20:43 +0100

    Added menu buttons and fujit as well as page scrolling

--- src/components/Header/MobileHeader/MobileHeader.js
+++ src/components/Header/MobileHeader/MobileHeader.js
@@ -1,11 +1,23 @@
 import { useState } from "react";
 
-import { StyledMobileHeader, Menu, MenuBackdrop } from "./MobileHeader.styles";
+import {
+  StyledMobileHeader,
+  Menu,
+  MenuBackdrop,
+  StyledContactButton,
+  StyledPinkFujit,
+  StyledContactUs,
+} from "./MobileHeader.styles";
 import { CompactLogo } from "../../Logo/CompactLogo/CompactLogo";
 import { MenuButton } from "../../Menu/MenuButton/MenuButton";
+import pinkPentagonFujit from "../../../images/fujits/pink-pentagon-fujit.svg";
 
 export const MobileHeader = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleMenuClick = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   return (
     <div>
@@ -15,7 +27,20 @@ export const MobileHeader = () => {
       </StyledMobileHeader>
       {isMenuOpen && (
         <>
-          <MenuBackdrop /> <Menu>HelloWorld</Menu>
+          <MenuBackdrop />
+          <Menu>
+            <a onClick={handleMenuClick} href="#what-we-do">
+              What we do
+            </a>
+            <a onClick={handleMenuClick} href="#who-are-we">
+              Who are we
+            </a>
+            <hr />
+            <StyledContactUs>
+              <StyledContactButton text="Say Hello" />
+              <StyledPinkFujit src={pinkPentagonFujit} />
+            </StyledContactUs>
+          </Menu>
         </>
       )}
     </div>
