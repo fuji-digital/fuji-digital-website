70a73a95790d436bc25b6b68604aa5f18a45202c
Greg Carter <gregamonintendo@gmail.com>
2024-07-10 15:53:45 +0100

    Updated Model form

--- src/components/EmailForm/EmailForm.js
+++ src/components/EmailForm/EmailForm.js
@@ -1,93 +1,75 @@
 import { useState } from "react";
-import { StyledModal, StyledInputBox } from "./EmailForm.styles";
-import { render } from "@react-email/components";
-import { SES } from "@aws-sdk/client-ses";
-import { FujiDigitalEmail } from "./fuji-digital-email";
+
+import {
+  StyledModal,
+  StyledInputBox,
+  StyledButton,
+  StyledButtonContainer,
+  StyledForm,
+  StyledFujit,
+} from "./EmailForm.styles";
+
+import pinkPentagonFujit from "../../images/fujits/pink-pentagon-fujit.svg";
 
 export const EmailForm = () => {
-  const ses = new SES({ region: process.env.AWS_SES_REGION });
-
-  const emailHtml = render(<FujiDigitalEmail />);
-
-  const sendEmail = async (email) => {
-    const params = {
-      Source: "fuji.digital.tech@gmail.com",
-      Destination: {
-        ToAddresses: [email],
-      },
-      Message: {
-        Body: {
-          Html: {
-            Charset: "UTF-8",
-            Data: emailHtml,
-          },
-        },
-        Subject: {
-          Charset: "UTF-8",
-          Data: "hello world",
-        },
-      },
-    };
-    await ses.sendEmail(params);
-  };
-
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    description: "",
-  });
-
-  const handleChange = (event) => {
-    const { id, value } = event.target;
-
-    setFormData((prevData) => {
-      return {
-        ...prevData,
-        [id]: value,
-      };
-    });
-  };
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
   const handleSubmit = (event) => {
+    const name = event.target[0].value;
+    const email = event.target[1].value;
+    const description = event.target[2].value;
+    const data = {
+      name,
+      email,
+      description,
+    };
+    setIsSubmitted(true);
+
     event.preventDefault();
-    sendEmail(formData.email);
   };
 
   return (
     <StyledModal>
       <h3>Contact Us</h3>
-      <form onSubmit={handleSubmit}>
-        <StyledInputBox>
-          <label for="name">Name</label>
-          <input
-            type="text"
-            id="name"
-            placeholder="Enter name here..."
-            onChange={handleChange}
-          ></input>
-        </StyledInputBox>
+      <StyledFujit src={pinkPentagonFujit} alt="A Pink mascot waving" />
+      {isSubmitted ? (
+        <p>
+          Thank you for submitting your question, our Fujits are working on an
+          answer right away 😊
+        </p>
+      ) : (
+        <StyledForm onSubmit={handleSubmit}>
+          <StyledInputBox>
+            <label for="name">Name</label>
+            <input
+              type="text"
+              id="name"
+              placeholder="Enter name here..."
+            ></input>
+          </StyledInputBox>
 
-        <StyledInputBox>
-          <label for="email">Email</label>
-          <input
-            type="email"
-            id="email"
-            placeholder="Enter email here..."
-            onChange={handleChange}
-          ></input>
-        </StyledInputBox>
+          <StyledInputBox>
+            <label for="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              placeholder="Enter email here..."
+            ></input>
+          </StyledInputBox>
 
-        <StyledInputBox>
-          <label for="description">How can we help?</label>
-          <textarea
-            type="textarea"
-            id="description"
-            placeholder="Enter question here..."
-            onChange={handleChange}
-          ></textarea>
-        </StyledInputBox>
-        <button>Hello</button>
-      </form>
+          <StyledInputBox>
+            <label for="description">How can we help?</label>
+            <textarea
+              type="textarea"
+              id="description"
+              placeholder="Enter question here..."
+            ></textarea>
+          </StyledInputBox>
+          <StyledButtonContainer>
+            <StyledButton>Submit</StyledButton>
+          </StyledButtonContainer>
+        </StyledForm>
+      )}
     </StyledModal>
   );
 };
