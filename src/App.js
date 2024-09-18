15b711dfdcac1519a17155e5c0d2b6bc6edda4e3
Greg Carter <gregamonintendo@gmail.com>
2024-09-18 14:30:43 +0100

    removed the app test and set to live

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-import { MaintenancePage } from "./components/MaintenancePage/MaintenancePage";
 import { LivePage } from "./components/LivePage/LivePage";
 import { GlobalStyle } from "./App.styles";
 
 export const App = () => {
-  const { REACT_APP_DEV } = process.env;
-  const isLive = REACT_APP_DEV;
-
-  return isLive ? (
+  return (
     <>
       <GlobalStyle />
       <LivePage />
     </>
-  ) : (
-    <MaintenancePage />
   );
 };
