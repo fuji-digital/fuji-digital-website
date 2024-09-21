7eb5fcd711d203de6906d3b3ae515fe12f06ff6c
Greg Carter <gregamonintendo@gmail.com>
2024-09-21 15:47:43 +0100

    Added x and y scroll animations to page

--- src/components/WhatWeDo/WhatWeDo.js
+++ src/components/WhatWeDo/WhatWeDo.js
@@ -1,3 +1,5 @@
+import { motion, useScroll, useTransform } from "framer-motion";
+
 import {
   StyledHeader,
   StyledTextContainer,
@@ -18,50 +20,85 @@ import graphqlIcon from "../../images/icons/graphql-icon.svg";
 import nodeIcon from "../../images/icons/node-js-icon.svg";
 
 export const WhatWeDo = () => {
+  const { scrollYProgress } = useScroll();
+
+  const fadeInOnScrollInformation = useTransform(
+    scrollYProgress,
+    [0, 0.35],
+    [0.5, 1]
+  ); // Fade in
+
+  const fadeInOnScrollYInformation = useTransform(
+    scrollYProgress,
+    [0, 0.35],
+    [70, 0]
+  );
+
+  const fadeInOnScrollIcon = useTransform(scrollYProgress, [0.1, 0.55], [0, 1]); // Fade in
+
+  const fadeInOnScrollYIcon = useTransform(
+    scrollYProgress,
+    [0.1, 0.55],
+    [100, 0]
+  );
+
   return (
     <StyledBody id="what-we-do">
       <StyledHeader>What We Do</StyledHeader>
-      <StyledInformationContainer>
-        <StyledTextContainer>
-          <StyledFujit src={pinkStripeFujit} />
-          <p>
-            At our software engineering company, we prioritize delivering
-            top-tier work for every website we create. We tailor our solutions
-            to meet the unique needs of each client, ensuring high-quality,
-            functional, and visually appealing results.
-          </p>
-        </StyledTextContainer>
-        <StyledReverseIconContainer>
-          <StyledFujit src={purpleTallFujit} />
-          <p>
-            We utilize the latest technologies to stay ahead of industry trends.
-            By leveraging cutting-edge tools and techniques, we guarantee
-            efficient, innovative, and secure websites that perform optimally
-            across all devices.
-          </p>
-        </StyledReverseIconContainer>
-        <StyledTextContainer>
-          <StyledFujit src={pinkRoundShortFujit} />
-          <p>
-            Our team prides itself on exceptional customer service and timely
-            project completion. We maintain open communication, ensuring clients
-            are informed and satisfied throughout the development process.
-          </p>
-        </StyledTextContainer>
-      </StyledInformationContainer>
-      <StyledIconContainer>
-        <img src={awsIcon} alt="AWS Icon" />
-        <img src={reactIcon} alt="React Icon" />
-        <img src={graphqlIcon} alt="GraphQL Icon" />
-        <img src={nodeIcon} alt="Node Icon" />
-      </StyledIconContainer>
-      <p>
-        Our software stack includes React for dynamic front-end development,
-        Node.js for robust back-end solutions, and JavaScript for seamless
-        integration. We utilize AWS for reliable cloud services and GraphQL for
-        efficient data management. Always eager to innovate, we continuously
-        explore and adopt new technologies to enhance our offerings.
-      </p>
+      <motion.div
+        style={{
+          opacity: fadeInOnScrollInformation,
+          x: fadeInOnScrollYInformation,
+        }}
+      >
+        <StyledInformationContainer>
+          <StyledTextContainer>
+            <StyledFujit src={pinkStripeFujit} />
+            <p>
+              At our software engineering company, we prioritize delivering
+              top-tier work for every website we create. We tailor our solutions
+              to meet the unique needs of each client, ensuring high-quality,
+              functional, and visually appealing results.
+            </p>
+          </StyledTextContainer>
+          <StyledReverseIconContainer>
+            <StyledFujit src={purpleTallFujit} />
+            <p>
+              We utilize the latest technologies to stay ahead of industry
+              trends. By leveraging cutting-edge tools and techniques, we
+              guarantee efficient, innovative, and secure websites that perform
+              optimally across all devices.
+            </p>
+          </StyledReverseIconContainer>
+          <StyledTextContainer>
+            <StyledFujit src={pinkRoundShortFujit} />
+            <p>
+              Our team prides itself on exceptional customer service and timely
+              project completion. We maintain open communication, ensuring
+              clients are informed and satisfied throughout the development
+              process.
+            </p>
+          </StyledTextContainer>
+        </StyledInformationContainer>
+      </motion.div>
+      <motion.div
+        style={{ opacity: fadeInOnScrollIcon, y: fadeInOnScrollYIcon }}
+      >
+        <StyledIconContainer>
+          <img src={awsIcon} alt="AWS Icon" />
+          <img src={reactIcon} alt="React Icon" />
+          <img src={graphqlIcon} alt="GraphQL Icon" />
+          <img src={nodeIcon} alt="Node Icon" />
+        </StyledIconContainer>
+        <p>
+          Our software stack includes React for dynamic front-end development,
+          Node.js for robust back-end solutions, and JavaScript for seamless
+          integration. We utilize AWS for reliable cloud services and GraphQL
+          for efficient data management. Always eager to innovate, we
+          continuously explore and adopt new technologies to enhance our
+          offerings.
+        </p>
+      </motion.div>
     </StyledBody>
   );
 };
